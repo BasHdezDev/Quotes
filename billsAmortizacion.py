ea8cfa34cce446aa4b23a36a7094b79d1ac4c826
Bas <bysebashdez@gmail.com>
2023-08-18 05:57:40 -0500

    Changed a few implementations on billsAmortizacion

--- billsAmortizacion.py
+++ billsAmortizacion.py
@@ -6,34 +6,33 @@ def interes_total(monto,tasa,cuotas):
     return total_intereses
 
 def amortizacion(monto, tasa, cuotas):
-    valor_cuota = round(billsCalculator.monthly_bills(monto, tasa, cuotas), 2)
+    valor_cuota = billsCalculator.monthly_bills(monto, tasa, cuotas)
     print(valor_cuota)
+    interes_x = tasa/100
     saldo = monto
     tabla_amortizacion = [["Cuota", "Saldo", "Pago interés", "Abono capital"], ["#", valor_cuota, tasa, monto]]
     if cuotas == 1:
         numero_cuota = 1
-        interes = round((tasa * saldo) / 100, 2)
-        abono_capital = round(valor_cuota - interes, 2)
+        interes = (tasa * saldo) / 100
+        abono_capital = valor_cuota - interes
         fila = [numero_cuota, saldo, interes, abono_capital]
         tabla_amortizacion.append(fila)
     else:
         for cuota in range(1, cuotas + 1):
             numero_cuota = cuota
-            interes = round((tasa * saldo) / 100, 2)
-            abono_capital = round(valor_cuota - interes, 2)
-            saldo = round(saldo - abono_capital, 2)
+            interes = interes_x * saldo
+            abono_capital = valor_cuota - interes
+            saldo = saldo - abono_capital
             
 
             fila = [numero_cuota, saldo, interes, abono_capital]
-            if fila[1] < 0:
-                fila[1] = 0
             tabla_amortizacion.append(fila)
             print(fila)
 
     return tabla_amortizacion
 
-monto = 200000
-cuotas = 36
-tasa_interes = 3.10
+monto = 850000
+cuotas = 24
+tasa_interes = 3.4
 
 print(amortizacion(monto, tasa_interes, cuotas))
