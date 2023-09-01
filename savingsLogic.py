ae137e556fe08aac204348e38b2daa3e482f085b
Bas <bysebashdez@gmail.com>
2023-09-01 06:03:52 -0500

    Created a first implementation

---
+++ savingsLogic.py
@@ -0,0 +1,23 @@
+class Savings:
+   def __init__(self,amount: float,interest,months:int):
+      self.amount = amount
+      self.interest = interest
+      self.months = months
+      self.real_interest = self.interest/100
+      self.savings_so_far = 0
+      self.interest_so_far = 0
+
+   def total_savings(self):
+      for savings in range(self.months):
+         self.savings_so_far += round(self.amount+self.interest_so_far,4)
+         self.interest_so_far = self.savings_so_far*self.real_interest
+         print(self.savings_so_far)
+      return round(self.savings_so_far,2)
+
+   def goal_savings(self):
+      pass
+
+
+x = Savings(9000,0.7,36)
+
+print(x.total_savings())
---
+++ savingsTests.py
@@ -0,0 +1,12 @@
+import unittest
+import savingsLogic
+
+class testSavings(unittest.TestCase):
+    def testTotalSavings(self):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    unittest.main() 
